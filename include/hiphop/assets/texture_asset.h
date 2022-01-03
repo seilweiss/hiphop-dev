3e8217d67c6440a2aec29930ae02c3c5ed103235
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2022-01-02 21:44:18 -0600

    impl TextureAsset

--- include/hiphop/assets/texture_asset.h
+++ include/hiphop/assets/texture_asset.h
@@ -15,8 +15,14 @@ namespace HipHop {
         TextureAsset(Asset asset) : RenderWareAsset(asset) {}
 
 #ifdef HIPHOP_USE_RWS
+        Rws::TexDictionary* GetTexDictionary() const { return m_texDictionary; }
+        void SetTexDictionary(Rws::TexDictionary* texDictionary);
+
         void Read(Stream* stream);
         void Write(Stream* stream);
+
+    private:
+        Rws::TexDictionary* m_texDictionary = nullptr;
 #endif
     };
 
--- src/assets/texture_asset.cpp
+++ src/assets/texture_asset.cpp
@@ -3,9 +3,27 @@
 namespace HipHop {
 
 #ifdef HIPHOP_USE_RWS
+    void TextureAsset::SetTexDictionary(Rws::TexDictionary* texDictionary)
+    {
+        SetChunk(texDictionary);
+
+        m_texDictionary = texDictionary;
+    }
+
     void TextureAsset::Read(Stream* stream)
     {
         RenderWareAsset::Read(stream);
+
+        Rws::Chunk* chunk = GetChunk();
+
+        if (chunk)
+        {
+            m_texDictionary = chunk->As<Rws::TexDictionary>();
+        }
+        else
+        {
+            m_texDictionary = nullptr;
+        }
     }
 
     void TextureAsset::Write(Stream* stream)
